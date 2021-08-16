d7efc97de4397da5feb97e263870e7d989aed162
guisiq <63610173+guisiq@users.noreply.github.com>
2021-08-16 10:50:40 -0300

    init spring boot

---
+++ api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/ApiSigLabItpacApplication.java
@@ -0,0 +1,13 @@
+package br.com.itpacpalmas.api_sig_lab_itpac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSigLabItpacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSigLabItpacApplication.class, args);
+	}
+
+}
---
+++ api_sig_lab_itpac/src/test/java/br/com/itpacpalmas/api_sig_lab_itpac/ApiSigLabItpacApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itpacpalmas.api_sig_lab_itpac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSigLabItpacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
