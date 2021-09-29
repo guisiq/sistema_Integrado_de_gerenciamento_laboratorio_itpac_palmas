93ce400622f9439e9852a480406cbb14297d83e8
@luanamacedo <luanamacedo@unitins.br>
2021-09-29 12:13:53 -0300

    Classes Manual e Sala

---
+++ api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/model/Sala.java
@@ -0,0 +1,80 @@
+package br.com.itpacpalmas.api_sig_lab_itpac.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Sala {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 60)
+    private String nome;
+
+    @Column( length = 100)
+    private String local;
+
+    private Integer capacidade;
+
+    @Column (length = 300)
+    private String descricao;
+
+    @Column(nullable = false)
+    private boolean ativo = true;
+
+   
+ //------------Getter e Setter
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return this.local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+    public Integer getCapacidade() {
+        return this.capacidade;
+    }
+
+    public void setCapacidade(Integer capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isAtivo() {
+        return this.ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+    
+}
