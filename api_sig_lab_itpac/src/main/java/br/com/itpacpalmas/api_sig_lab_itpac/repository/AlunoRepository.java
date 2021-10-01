819038213c97a0c56fe0b638e235e3ab2187de19
guisiq <lopesgui01@hotmail.com>
2021-10-01 13:13:04 -0300

    + as interfaces JpaRepository para clases do model

--- api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/repository/AlunoRepository.java
+++ api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/repository/AlunoRepository.java
@@ -5,6 +5,5 @@ package br.com.itpacpalmas.api_sig_lab_itpac.repository;
 import br.com.itpacpalmas.api_sig_lab_itpac.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubgrupoRepository extends JpaRepository<Subgrupo, Integer> {
-
+public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
 }
