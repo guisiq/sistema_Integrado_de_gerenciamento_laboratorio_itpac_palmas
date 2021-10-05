6c3a9277641aeeeeb091dd91f01e29d559c14ae7
guisiq <lopesgui01@hotmail.com>
2021-10-05 16:05:18 -0300

    salvando auteracoes

--- api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/Services/ManualServiss.java
+++ api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/Services/ManualServiss.java
@@ -1,14 +1,18 @@
 package br.com.itpacpalmas.api_sig_lab_itpac.Services;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import br.com.itpacpalmas.api_sig_lab_itpac.controller.ManualController;
 import br.com.itpacpalmas.api_sig_lab_itpac.entities.VO.ManualInfo;
 import br.com.itpacpalmas.api_sig_lab_itpac.repository.ManualRepository;
-
+@Service
 public class ManualServiss {
     @Autowired
     ManualRepository repo;
@@ -22,9 +26,26 @@ public class ManualServiss {
         return ManualInfo.convert(repo.findById(Id).orElse(null)); 
         
     }
-
-    public MultipartFile buscarDocbyId() {
-        return null;
-    }
     
+/*
+    public ResponseEntity<?> buscarDocbyId(int id) {
+         // Sanitizar... (biblioteca, nomeDoArquivo.startsWith, regex)
+       var manual = repo.findById(id).orElse(null);
+       var filename = String.format("/home/geovaniggr/home/study/%s.pdf", manual.getDocumento());
+
+       try {
+           var file = new File(filename);
+           var path = Paths.get(file.getAbsolutePath());
+           var resource = new ByteArrayResource(Files.readAllBytes(path));
+           return ResponseEntity
+                   .ok()
+                   .contentType(MediaType.APPLICATION_PDF)
+                   .contentLength(file.length())
+                   .body(resource);
+       } catch (IOException e) {
+           e.printStackTrace();
+           return ResponseEntity.notFound().build();
+       }
+    }
+*/    
 }
--- api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/ManualController.java
+++ api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/ManualController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,8 +40,8 @@ public class ManualController {
   
     // GET      ⇒\api\{V}\manual\doc\{ID}
     @GetMapping("/doc/{id}")
-    public MultipartFile getDocById() {
-        return servises.buscarDocbyId();
+    public ResponseEntity<?> getDocById(@PathVariable int id) {
+      return null;
     }
   
     // POST    ⇒\api\{V}\manual\
