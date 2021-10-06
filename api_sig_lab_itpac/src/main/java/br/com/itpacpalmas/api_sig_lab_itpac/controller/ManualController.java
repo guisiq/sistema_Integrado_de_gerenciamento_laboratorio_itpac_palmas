b2b1946666377bd2cf458203efcafa73e8f53abd
guisiq <lopesgui01@hotmail.com>
2021-10-05 22:49:31 -0300

    manual

--- api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/ManualController.java
+++ api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/ManualController.java
@@ -1,24 +1,32 @@
 package br.com.itpacpalmas.api_sig_lab_itpac.controller;
 
+import java.io.File;
 import java.util.List;
 import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.itpacpalmas.api_sig_lab_itpac.Services.ManualServiss;
+import br.com.itpacpalmas.api_sig_lab_itpac.Services.FileStorageService;
+import br.com.itpacpalmas.api_sig_lab_itpac.entities.Manual;
 import br.com.itpacpalmas.api_sig_lab_itpac.entities.PerfilUser;
-import br.com.itpacpalmas.api_sig_lab_itpac.entities.VO.ManualInfo;
+import br.com.itpacpalmas.api_sig_lab_itpac.entities.VO.ManualResponseVO;
+//import br.com.itpacpalmas.api_sig_lab_itpac.entities.VO.ManualInfo;
 import br.com.itpacpalmas.api_sig_lab_itpac.repository.PerfilUserRepository;
 
 // https://www.youtube.com/watch?v=DtC_KfU6b1o
@@ -26,37 +34,36 @@ import br.com.itpacpalmas.api_sig_lab_itpac.repository.PerfilUserRepository;
 @RequestMapping("api/manual")
 public class ManualController {
     @Autowired
-    ManualServiss servises;
-    // GET      ⇒/api/{V}/manual/info
-    @GetMapping("/info")
-    public  List<ManualInfo> getInfo() {
+	private FileStorageService servises;
+    
+    @GetMapping("")
+    public  List<ManualResponseVO> getInfo() {
         return servises.buscarTodosInfo();
     }
-    // GET      ⇒\api\{V}\manual\info\{ID}
-    @GetMapping("/info/{id}")
-    public ManualInfo getInfoById(@PathVariable int id) {
+    
+    @GetMapping("{id}")
+    public ManualResponseVO getInfoById(@PathVariable int id) {
         return servises.buscarbyIdInfo(id );
     }
-  
-    // GET      ⇒\api\{V}\manual\doc\{ID}
+    
     @GetMapping("/doc/{id}")
-    public ResponseEntity<?> getDocById(@PathVariable int id) {
-      return null;
+    public ResponseEntity<?> getDocById(@PathVariable int id, HttpServletRequest request) {
+        return servises.downloadFile(id, request);
     }
-  
-    // POST    ⇒\api\{V}\manual\
+    
     @PostMapping
-    public void creat() {
-        
+	public ManualResponseVO uploadFile(@RequestParam("file") MultipartFile file,@RequestParam("descricao") String descricao) {
+        return servises.uploadFile(file,descricao);
     }
-    // PUT      ⇒\api\{V}\manual\{ID}
-    @PutMapping
-    public void update() {
-        
+    
+    @PatchMapping("/{id}/{descricao}")
+    public void update(@PathVariable int id,@PathVariable String descricao) {
+        servises.update(id, descricao);
     }
-// delete  ⇒\api\{V}\manual\{ID}
-    @DeleteMapping
-    public void delet() {
-        
+
+    @DeleteMapping("/{id}")
+    public void delet(@PathVariable int id) {
+       servises.delete(id); 
     }
+
 }
