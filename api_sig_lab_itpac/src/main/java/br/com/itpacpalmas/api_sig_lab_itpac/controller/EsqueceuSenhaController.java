03595352028870d52be4616154482b045d27966d
Davi Noleto <davidnpjs@gmail.com>
2021-12-01 21:25:55 -0300

    finalizado recuperar senha

--- api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/EsqueceuSenhaController.java
+++ api_sig_lab_itpac/src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/EsqueceuSenhaController.java
@@ -5,17 +5,18 @@ import java.time.LocalDateTime;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.itpacpalmas.api_sig_lab_itpac.Services.EmailService;
 import br.com.itpacpalmas.api_sig_lab_itpac.entities.RecuperarSenha;
 import br.com.itpacpalmas.api_sig_lab_itpac.entities.Usuario;
 import br.com.itpacpalmas.api_sig_lab_itpac.repository.RecuperarSenhaRepository;
 import br.com.itpacpalmas.api_sig_lab_itpac.repository.UsuarioRepository;
+import br.com.itpacpalmas.api_sig_lab_itpac.services.EmailService;
 
 @RestController
 @RequestMapping(value="api/forgotpass")
@@ -27,41 +28,26 @@ UsuarioRepository usuarioRepository;
 RecuperarSenhaRepository recuperarSenhaRepository;
 
 	@GetMapping
-	public boolean enviarEmail(@RequestParam(name = "email") String email) {
-		Usuario usuario = null;
-		try {
-			usuario = usuarioRepository.findByEmail(email);
-		} catch (Exception e) {
-			return false;
-		}
-		// gerando codigo aleatorio
+	public ResponseEntity<?> enviarEmail(@RequestParam(name = "email") String email) {
+		 
+		Usuario usuario = usuarioRepository.findByEmail(email);
 		Random r = new Random();
 		DecimalFormat format = new DecimalFormat("Afya-000000");
 		String codigo = format.format(r.nextInt(1000000));
-		
 		RecuperarSenha entity = new RecuperarSenha();
 		entity.setCodigo(codigo);
 		entity.setUsuario(usuario);
 		entity.setUtilizado(false);
-		// definindo 24 horas de tempo limite
 		entity.setDataLimite(LocalDateTime.now().plusDays(1));
-		
-		try {
-			recuperarSenhaRepository.save(entity);
-			EmailService mail = new EmailService(usuario.getPessoa().getEmail(), 
-									"Esqueceu a Senha",
-									codigo+" é seu código de recuperação de senha.");
-			mail.enviar();
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+		recuperarSenhaRepository.save(entity);
+		EmailService mail = new EmailService(usuario.getPessoa().getEmail(), "Esqueceu a Senha",codigo+" é seu código de recuperação de senha.");
+		mail.enviar();
+		return ResponseEntity.ok().body("Email enviado!");
 		
 	}
 	
 	@GetMapping(value="/alterpass")
-	public Usuario alterarSenha(@RequestParam String codigo,@RequestParam String senha) {
+	public ResponseEntity<?> alterarSenha(@RequestParam String codigo,@RequestParam String senha) {
 	
 		RecuperarSenha obj = recuperarSenhaRepository.findByCodigo(codigo);
 		if(obj == null || obj.getDataLimite().isBefore(LocalDateTime.now()) || obj.getUtilizado() == true) {
@@ -70,15 +56,14 @@ RecuperarSenhaRepository recuperarSenhaRepository;
 		}
 		else {
 			Usuario usu = obj.getUsuario();
-			System.out.println(usu.getPassword());
-			usu.setPassword(senha);
+			BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
+			usu.setPassword(bCryptPasswordEncoder.encode(senha));
 			obj.setUtilizado(true);
-			Usuario teste =usuarioRepository.save(usu);
-			System.out.println(teste.getPassword());
+			usuarioRepository.save(usu);
 			recuperarSenhaRepository.save(obj);
 			
 				
-			return teste;
+			return ResponseEntity.ok().body("Senha alterada!");
 		}
 		
 		
